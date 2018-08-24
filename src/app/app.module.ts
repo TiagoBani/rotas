11899845adcfb3c57004e9a2dd3c724026eb9a4f
tiago <tiagobani@github.com>
2018-08-24 00:05:32 -0300

    adicionado codigo retirado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
-
+// import { CursosComponent } from './cursos/cursos.component';
+// import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+// import { CursosService } from './cursos/cursos.service';
+// import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
     CabecalhoComponent,
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +34,7 @@ import { CabecalhoComponent } from './cabecalho/cabecalho.component';
     RoutingModule,
     CursosModule
   ],
-  providers: [],
+  // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
