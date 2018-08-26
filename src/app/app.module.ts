5a69df43c615a3db941fa5a0574f708cecafd1bc
tiago <tiagobani@github.com>
2018-08-25 22:26:41 -0300

    implementado login e guarda de rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { MaterializeModule } from 'angular2-materialize';
 
+import { AppRoutingModule } from './app.routing.module';
+import { AuthGuard } from './guards/auth.guard';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { AuthService } from './login/auth.service';
 
 // import { routing } from './app.routing';
 // import { AppRoutingModule } from './app.routing.module';
 // import { CursosModule } from './cursos/cursos.module';
 // import { CursosRoutingModule } from './cursos/cursos.routing.module';
 // import { AlunosModule } from './alunos/alunos.module';
-import { AppRoutingModule } from './app.routing.module';
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 // import { CursosService } from './cursos/cursos.service';
@@ -39,6 +41,7 @@ import { AppRoutingModule } from './app.routing.module';
     // CursosModule,
     // AlunosModule
   ],
+  providers: [AuthService, AuthGuard],
   // providers: [CursosService],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,14 +5,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 // import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 // import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 
 const appRoutes: Routes = [
-    {path: 'cursos', loadChildren: './cursos/cursos.module#CursosModule'},
-    {path: 'alunos', loadChildren: './alunos/alunos.module#AlunosModule'},
-    { path: '', component: HomeComponent},
+    { path: 'cursos', canActivate: [AuthGuard],
+        loadChildren: './cursos/cursos.module#CursosModule'},
+    { path: 'alunos', canActivate: [AuthGuard],
+        loadChildren: './alunos/alunos.module#AlunosModule'},
     { path: 'login', component: LoginComponent},
+    { path: '', canActivate: [AuthGuard],
+        component: HomeComponent},
     // { path: 'cursos', component: CursosComponent},
     // { path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
     // { path: 'curso/:id', component: CursoDetalheComponent}
